bef0967bf2cc4a4f84e1caaf387ccf16a3a6e0d6
shaw342 <baruashawan41@gmail.com>
2023-05-22 14:16:09 +0200

    add bubble sort.h

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+void swap(int *xp, int *yp)
+{
+	int *tmp;
+
+	*tmp = *xp;
+	*xp = *yp;
+	*yp = *tmp;
+}
+void bubble_sort(int *array, size_t size)
+{
+	int step;
+	int i;
+
+	for (step = 0; step < size - 1;++step)
+	{
+		for (i = 0; i < size - step - 1; ++i)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(&array[i],&array[i + 1]);
+			}
+
+		}
+	}
+}
---
+++ sort.h
@@ -0,0 +1,6 @@
+#ifndef __SORT__
+#define __SORT__
+#include <stdio.h>
+#include <stdlib.h>
+void bubble_sort(int *array, size_t size);
+#endif
