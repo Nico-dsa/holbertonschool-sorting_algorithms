d112fe7488b749f3f77502de300b034219d6d327
shaw342 <baruashawan41@gmail.com>
2023-05-23 03:58:43 +0200

    1-insertion_sort_list.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,10 @@
 #include "sort.h"
+
+/**
+ * swap - function swap
+ * @xp: imput
+ * @yp: imput
+ */
 void swap(int *xp, int *yp)
 {
 	int tmp;
@@ -7,18 +13,28 @@ void swap(int *xp, int *yp)
 	*xp = *yp;
 	*yp = tmp;
 }
+
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * @array: array
+ * @size: size
+ */
 void bubble_sort(int *array, size_t size)
 {
-	int step;
-	int i;
+	size_t step;
+	size_t i;
 
-	for (step = 0; step < size - 1;++step)
+	if (size < 2)
+		return;
+
+	for (step = 0; step < size - 1; step++)
 	{
 		for (i = 0; i < size - step - 1; ++i)
 		{
 			if (array[i] > array[i - 1])
 			{
-				swap(&array[i],&array[i + 1]);
+				swap(&array[i], &array[i + 1]);
+				print_array(array, size);
 			}
 
 		}
