453b2b36fe2482f62c2a03fde890098274e7842e
Nico-dsa <6463@holbertonstudents.com>
2023-05-25 02:10:11 -0700

    description to function that sorts an array of integers in ascending order

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,10 @@
 #include "sort.h"
+
+/**
+ * swap - function swap
+ * @xp: imput
+ * @yp: imput
+ */
 void swap(int *xp, int *yp)
 {
 	int tmp;
@@ -8,6 +14,15 @@ void swap(int *xp, int *yp)
 	*yp = tmp;
 }
 
+/**
+ * partition - Partitions an array of integers using the last element as pivot
+ * @array: The array to be partitioned.
+ * @low: The starting index of the range to be partitioned.
+ * @high: The ending index of the range to be partitioned.
+ * @size: The size of the array.
+ *
+ * Return The final index of the pivot after partitioning.
+ */
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
@@ -28,6 +43,14 @@ int partition(int *array, int low, int high, size_t size)
 	}
 	return (i);
 }
+
+/**
+ * quick_recursion - Performs the recursive steps of the quicksort algorithm
+ * @array: The array to be sorted.
+ * @low: The starting index of the range to be sorted.
+ * @high: The ending index of the range to be sorted.
+ * @size: The size of the array.
+ */
 void quick_recursion(int *array, int low, int high, size_t size)
 {
 	int pivot;
@@ -40,6 +63,12 @@ void quick_recursion(int *array, int low, int high, size_t size)
 
 	}
 }
+
+/**
+ * quick_sort - function that sorts an array of integers in ascending order
+ * @array: array
+ * @size: size
+ */
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL)
